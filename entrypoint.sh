637d4716c8fac0747a4b2f2cb1e95ef4dba46caf
José Luis Lafuente <jl@lafuente.me>
2020-04-13 14:45:01 +0200

    Fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,15 @@ fi
 project_name=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
 jar_name="${project_name}-${version}.jar"
 
+clojure -Spom
+
 mvn versions:set versions:set-scm-tag -DnewVersion="${version}" \
   -DnewTag="${version}" -DgenerateBackupPoms=false
 
-clojure -Spom
 clojure -Sdeps '{:deps {seancorfield/depstar {:mvn/version "1.0.94"}}}' \
   -m hf.depstar.jar target/"${jar_name}" -v
 
-mvn deploy:deploy-file -Dfile="${jar_name}" -DpomFile=pom.xml \
+mvn deploy:deploy-file -Dfile="target/${jar_name}" -DpomFile=pom.xml \
   -DrepositoryId=clojars -Durl=https://clojars.org/repo/ \
   -Dclojars.username="${CLOJARS_USERNAME}" \
   -Dclojars.password="${CLOJARS_PASSWORD}"
