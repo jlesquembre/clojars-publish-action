02597b6ea9bc3e5e56c127f4929fddbd4cf45ac2
José Luis Lafuente <jl@lafuente.me>
2020-07-01 18:56:20 +0200

    Better docker image

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,7 @@
-FROM nixery.dev/shell/maven/clojure
-
-ENV HOME /builder
-ENV _JAVA_OPTIONS -Duser.home=/builder
+FROM clojars-releaser-tmp:latest
 
 # Download deps
-RUN mkdir -p /tmp/maven \
-    && cd /tmp/maven \
-    && clojure -Sdeps '{:deps {seancorfield/depstar {:mvn/version "1.0.94"}}}' -Spom \
+RUN cd /tmp/maven \
     && mvn versions:help deploy:help \
     && mvn dependency:go-offline \
     && rm -rf /tmp/maven
-
-COPY entrypoint.sh /entrypoint.sh
-COPY settings.xml /builder/.m2/settings.xml
-
-ENTRYPOINT ["bash", "/entrypoint.sh"]
--- container.nix
+++ container.nix
@@ -1,30 +1,43 @@
-{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/22a81aa5fc15b2d41b12f7160a71cd4a9f3c3fa1.tar.gz) { }
-}:
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/22a81aa5fc15b2d41b12f7160a71cd4a9f3c3fa1.tar.gz) { } }:
 let
   maven = pkgs.maven.override { jdk = pkgs.jdk11_headless; };
   clojure = pkgs.clojure.override { jdk11 = pkgs.jdk11_headless; };
-  entrypoint = ./entrypoint.sh;
-  mvn-settings = ./settings.xml;
-  home-dir = "/home/builder";
+  entrypoint = ./files/entrypoint.sh;
+  mvn-settings = ./files/settings.xml;
+  pom = ./files/pom.xml;
+  home-dir = "home/builder";
+
+  deps-img = pkgs.dockerTools.buildImage {
+    name = "clojure-maven";
+    tag = "latest";
+    contents = [
+      pkgs.coreutils
+      pkgs.bash
+      maven
+      clojure
+    ];
+  };
 in
 pkgs.dockerTools.buildImage {
-  name = "clojars-releaser";
+  name = "clojars-releaser-tmp";
   tag = "latest";
-  contents = [
-    pkgs.coreutils
-    pkgs.bash
-    maven
-    clojure
-  ];
+
+  fromImage = deps-img;
+
   config.Env = [
-    "HOME=${home-dir}"
-    "_JAVA_OPTIONS=-Duser.home=${home-dir}"
+    "HOME=/${home-dir}"
+    "_JAVA_OPTIONS=-Duser.home=/${home-dir}"
   ];
+
+  # runAsRoot = ''
   extraCommands = ''
     mkdir -p ${home-dir}/.m2
     cp ${mvn-settings} ${home-dir}/.m2/settings.xml
+
+    mkdir -p tmp/maven
+    cp ${pom} tmp/maven/pom.xml
   '';
-  config = {
-    Cmd = [ "${pkgs.bash}/bin/bash" ];
-  };
+
+  config.Cmd = [ "${pkgs.bash}/bin/bash" entrypoint ];
+  config.WorkingDir = "/${home-dir}";
 }
